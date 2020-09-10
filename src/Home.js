5f9b34dcdfcc2966473e6b09a8bcd13a903e4305
Zulfahmi-dev <fzul115@gmail.com>
2020-09-10 20:40:20 +0700

    first day create home page

---
+++ src/Home.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import './Home.css';
+import Product from './Product';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="" />
+
+                <div className="home__row" >
+                    <Product 
+                        title="The lean startup" 
+                        price={19.99} 
+                        image='https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg'
+                        rating={4}
+                    />
+                    <Product 
+                        title="ASUS TUF Gaming FX505DV" 
+                        price={500.99} 
+                        image='https://www.asus.com/media/global/products/R0aLUBgprfy023VC/P_setting_xxx_0_90_end_300.png'
+                        rating={5}
+                    />
+                    {/* Product components */}
+                </div>
+                <div className="home__row" >
+                    <Product 
+                        title="Amazon.in: Buy Epson L130 Single-Function Ink Tank Colour Printer Online at  Low Prices in India | Epson Reviews & Ratings" 
+                        price={500.99} 
+                        image='https://images-na.ssl-images-amazon.com/images/I/71FDAbUf9VL._SX425_.jpg'
+                        rating={5}
+                    />
+                    <Product 
+                        title="The lean startup" 
+                        price={19.99} 
+                        image='https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg'
+                        rating={4}
+                    />
+                    <Product 
+                        title="ASUS TUF Gaming FX505DV" 
+                        price={500.99} 
+                        image='https://cf.shopee.co.id/file/75bc51ce590714cbb9fb1298d32cb1a2'
+                        rating={5}
+                    />
+                </div>
+                <div className="home__row" >
+                    <Product 
+                        title="Microsoft Xbox One X 1Tb Console With Wireless Controller: Xbox One X  Enhanced, Hdr, Native 4K, Ultra Hd (Discontinued)" 
+                        price={19.99} 
+                        image='https://images-na.ssl-images-amazon.com/images/I/61ux1cU49XL._SX385_.jpg'
+                        rating={4}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEqTglCw2wpFhMqPrPr1yf7gagEfVt9dU",
+    authDomain: "clone-96952.firebaseapp.com",
+    databaseURL: "https://clone-96952.firebaseio.com",
+    projectId: "clone-96952",
+    storageBucket: "clone-96952.appspot.com",
+    messagingSenderId: "264250310247",
+    appId: "1:264250310247:web:809ea0869defdcc8199ec2",
+    measurementId: "G-NMXN0P4CLS"
+  };
